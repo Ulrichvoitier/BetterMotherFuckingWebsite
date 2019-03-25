80d8cebb7edd6ab436d1b349383d5807f7ac4569
Ulrichvoitier <ulrichvoitier@yahoo.fr>
2019-03-25 15:27:01 +0100

    essai.lien

--- script.js
+++ script.js
@@ -1,11 +1,13 @@
-<script type="text/javascript">
-	window.onload = function () {
-	document.getElementById('similipopup').style.display = "block";
-			    };			
-</script> 
-
+<script langage="javascript">
+function window.open (https://www.youtube.com/watch?v=dQw4w9WgXcQ)) 
+{window.opener.document.location = https://www.youtube.com/watch?v=dQw4w9WgXcQ);
+window.close();
+}
 var xxxvidsxxx = document.getElementById("xxxvidsxxx");
 var btnClose = document.getElementById("btnClose");
 var btnOpen = document.getElementById("btnOpen") window.open("https://www.youtube.com/watch?v=dQw4w9WgXcQ"
 "toolbar=yes, status=yes, scrollbars=yes, resizable=yes, 
 width=700, height=500,";
+
+</script>
+
