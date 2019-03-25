c50a5438b972c4edf35cd6543d96554ac774cf2e
Ulrichvoitier <ulrichvoitier@yahoo.fr>
2019-03-25 15:00:21 +0100

    essai popup.2

---
+++ script.js
@@ -0,0 +1,11 @@
+<script type="text/javascript">
+	window.onload = function () {
+	document.getElementById('similipopup').style.display = "block";
+			    };			
+</script> 
+
+var xxxvidsxxx = document.getElementById("xxxvidsxxx");
+var btnClose = document.getElementById("btnClose");
+var btnOpen = document.getElementById("btnOpen") window.open("https://www.youtube.com/watch?v=dQw4w9WgXcQ"
+"toolbar=yes, status=yes, scrollbars=yes, resizable=yes, 
+width=700, height=500,";
