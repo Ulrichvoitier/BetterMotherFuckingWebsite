bf1298260b2218e2967c3a2f5a1e324594c92d3b
Ulrichvoitier <ulrichvoitier@yahoo.fr>
2019-03-29 16:59:23 +0100

    essai pop you

--- script.js
+++ script.js
@@ -1,17 +1,9 @@
-window.onload = setTimeout(function(){
-	var pop = alert('Hey le site xxxvidsxxx est super bien. Viens dessus stp please');
-	if (btnOpen == true) {
+window.onload = setTimeout(function(alert){
+	var pop = confirm('Hey le site xxxvidsxxx est super bien. Viens dessus stp please');
+	if (pop == true) {
 		window.location = "https://www.youtube.com/watch?v=dQw4w9WgXcQ";
 	}
 	else {
 		false;
 	}
 }10000)
-
-var  xxxvidsxxx = document.getElementById("xxxvidsxxx");
-var btnClose = document.getElementById("btnClose");
-var btnOpen = document.getElementById("btnOpen") window.open("https://www.youtube.com/watch?v=dQw4w9WgXcQ"
-"toolbar=yes, status=yes, scrollbars=yes, resizable=yes, width=700, height=500,";
-
-
-
