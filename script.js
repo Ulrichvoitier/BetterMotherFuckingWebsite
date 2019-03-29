9079c408b12ded979b12b8371f386799138da001
Ulrichvoitier <ulrichvoitier@yahoo.fr>
2019-03-29 16:01:38 +0100

    essai lien youtube

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 window.onload = setTimeout(function(){
-	var btnOpen = confirm('Hey le site xxxvidsxxx est super bien. Viens dessus stp please');
+	var pop = alert('Hey le site xxxvidsxxx est super bien. Viens dessus stp please');
 	if (btnOpen == true) {
 		window.location = "https://www.youtube.com/watch?v=dQw4w9WgXcQ";
 	}
